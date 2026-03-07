e53a004ef812872af8a59d8f247911c81ac6deb8
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2026-03-07 23:08:11 +0500

    Update api.ts

--- client/reactjs/src/configs/api.ts
+++ client/reactjs/src/configs/api.ts
@@ -1,8 +1,4 @@
-import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true  // ✅ send cookies
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
-
-export default api;
