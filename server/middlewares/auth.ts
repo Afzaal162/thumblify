a3a16fe937055ee0734c208c57399b7a4d9aeeaa
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2026-03-09 00:50:08 +0500

    Update auth.ts

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,9 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
+const protect = (req, res, next) => {
 
-const protect = (req: Request, res: Response, next: NextFunction) => {
-  const { isLoggedIn, userId } = req.session as any;
-  if (!isLoggedIn || !userId)
-    return res.status(401).json({ message: "You are not logged in" });
+  const { isLoggedIn, userId } = req.session;
+
+  if (!isLoggedIn || !userId) {
+    return res.status(401).json({
+      message: "You are not logged in"
+    });
+  }
 
   next();
 };
