09772787d37cb79d1b76169abd765ea7d28b35ee
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2026-03-10 21:12:21 +0500

    Update auth.ts

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,22 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 
-// Inline session typing
 interface SessionData {
   isLoggedIn?: boolean;
   userId?: string;
 }
 
-const protect = (
-  req: Request & { session?: SessionData },
-  res: Response,
-  next: NextFunction
-) => {
-  const { isLoggedIn, userId } = req.session || {};
-
-  if (!isLoggedIn || !userId) {
+const protect = (req: Request & { session: SessionData }, res: Response, next: NextFunction) => {
+  if (!req.session.isLoggedIn || !req.session.userId) {
     return res.status(401).json({ message: "You are not logged in" });
   }
-
   next();
 };
 
