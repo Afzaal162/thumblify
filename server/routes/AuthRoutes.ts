71383f80d0cf4dd58047ef62f39e50285f43fe64
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2026-03-10 21:06:40 +0500

    Update AuthRoutes.ts

--- server/routes/AuthRoutes.ts
+++ server/routes/AuthRoutes.ts
@@ -1,20 +1,13 @@
-import { Router } from "express";
-import {
-  registerUser,
-  loginUser,
-  logoutUser,
-  verifyUser
-} import { registerUser, loginUser, logoutUser, verifyUser } from "../controllers/AuthControllers";
-import protect from "../middlewares/auth";
+import express from "express";
+import { registerUser, loginUser, logoutUser, verifyUser } from "../controllers/AuthControllers.js";
+import protect from "../middlewares/auth.js";
 
-const router = Router();
+const router = express.Router();
 
-// Public routes
+// Auth Routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
-// Protected routes
-router.post("/logout", protect, logoutUser);
+router.post("/logout", logoutUser);
 router.get("/verify", protect, verifyUser);
 
 export default router;
