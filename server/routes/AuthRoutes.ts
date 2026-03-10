347f72ba7286803bc3fce50ce515cc04150cca91
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2026-03-10 21:11:07 +0500

    Update AuthRoutes.ts

--- server/routes/AuthRoutes.ts
+++ server/routes/AuthRoutes.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import { Router } from "express";
 import { registerUser, loginUser, logoutUser, verifyUser } from "../controllers/AuthControllers.js";
 import protect from "../middlewares/auth.js";
 
-const router = express.Router();
+const router = Router();
 
 // Auth Routes
 router.post("/register", registerUser);
