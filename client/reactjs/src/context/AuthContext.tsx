7aa942e228a764b2494b506d815065d38c7cdb1b
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2026-03-12 00:12:34 +0500

    Update AuthContext.tsx

--- client/reactjs/src/context/AuthContext.tsx
+++ client/reactjs/src/context/AuthContext.tsx
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     const verifyUser = async () => {
       try {
-        const res = await api.get("/verify");
+        const res = await api.get("/api/auth/verify");
         setUser(res.data.user);
       } catch {
         setUser(null);
